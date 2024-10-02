920510290b3e1f63237c957f6b1103485ba07ef1
unknown <anthony_rosati@yahoo.com>
2024-10-01 21:33:51 -0500

    Resume

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Navigation from "./Components/Navigation";
 export default function App() {
   return (
     <>
-      <header>
+      <header style={{ paddingTop: '1rem'}}>
         <h3>
           <Navigation />
         </h3>
       </header>
 
-      <main className="main">
+      <main className="main" style={{ paddingTop: '5rem'}}>
         <Outlet />
       </main>
     </>
--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,3 +1,54 @@
+import resumeDownload from "../assets/Anthony-Rosati-Resume.pdf";
+
 export default function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div>
+      {/* Downloadable resume */}
+      <h4 className="d-flex justify-content-center pb-3">
+        Download my resume&nbsp;
+        <a href={resumeDownload} download>
+          here.
+        </a>
+      </h4>
+
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div>
+          {/* Front end skills list */}
+          <div className="d-flex justify-content-center">
+            <h4>Front End Proficiencies</h4>
+          </div>
+          <div className="d-flex justify-content-center">
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>JQuery</li>
+              <li>Responsive Design</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+              <li>Prime React</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          {/* Back end skills list */}
+          <div className="d-flex justify-content-center">
+            <h4>Back End Proficiences</h4>
+          </div>
+          <div className="d-flex justify-content-center">
+            <ul>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL, Sequelize, MSSQL, Knex</li>
+              <li>MongoDB, Mongoose</li>
+              <li>REST</li>
+              <li>GraphQL</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
