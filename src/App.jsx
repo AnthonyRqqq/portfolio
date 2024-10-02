7d589f1f21df3aea71d6c763ddfd1e471c7cde1f
unknown <anthony_rosati@yahoo.com>
2024-10-01 19:19:12 -0500

    Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { Outlet } from 'react-router-dom'
 
 export default function App() {
   <>
+  <header>
+    <h3>
+      <Navigation />
+    </h3>
+  </header>
+
+
   <main className='main'>
     <Outlet />
   </main>
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,1 +1,29 @@
-export default function Navigation() {}
+import { Link, useLocation } from "react-router-dom";
+
+export default function Navigation() {
+  const currentPage = useLocation().pathName;
+  const pages = ["About", "Contact", "Portfolio", "Resume"];
+
+  return (
+    <div className="row">
+      <h1 className="col justify-content-start">Anthony Rosati</h1>
+
+      <ul className="nav col justify-content-end">
+        {pages.map((page, index) => {
+          return (
+            <li key={index} className="nav-item">
+              <Link
+                to={`/${page}`}
+                className={`${
+                  currentPage === "/" && "active"
+                } nav-link link-item`}
+              >
+                {page}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
