faaa896b42adb7124b8d5a8c6b22640bfea60e1c
unknown <anthony_rosati@yahoo.com>
2024-10-01 21:19:45 -0500

    Navbar styling complete

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,29 +1,38 @@
 import { Link, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import './Navigation.css'
 
 export default function Navigation() {
-  const currentPage = useLocation().pathName;
+  const currentPage = useLocation().pathname;
+  const navigate = useNavigate();
   const pages = ["About", "Contact", "Portfolio", "Resume"];
 
   return (
     <div className="row">
-      <h1 className="col justify-content-start">Anthony Rosati</h1>
+      <div>
+        <div className={currentPage === '/' ? 'homepage-navbar' : 'standard-navbar'}>
+          <h1 className="home-link" onClick={() => navigate('/')}>
+            Anthony Rosati
+          </h1>
 
-      <ul className="nav col justify-content-end">
-        {pages.map((page, index) => {
-          return (
-            <li key={index} className="nav-item">
-              <Link
-                to={`/${page}`}
-                className={`${
-                  currentPage === "/" && "active"
-                } nav-link link-item`}
-              >
-                {page}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+          <ul className="nav">
+            {pages.map((page, index) => {
+              return (
+                <li key={index} className="nav-item">
+                  <Link
+                    to={`/${page}`}
+                    className={`nav-link link-item ${
+                      currentPage === `/${page}` ? "active" : ""
+                    }`}
+                  >
+                    {page}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
