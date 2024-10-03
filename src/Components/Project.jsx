3eab7fba7b2e16f921961d79a06d9b01ac1ace29
unknown <anthony_rosati@yahoo.com>
2024-10-02 20:49:37 -0500

    Functional portfolio

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,41 +1,34 @@
-import './Project.css'
+import "./Project.css";
 
 export default function Project({ projects }) {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap'}}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
+      }}
+    >
       {projects.map((project) => (
-        <div key={project.id} className="project-card col-3 px-2 mx-3 my-4" >
+        <div key={project.id} className="project-card col-3 px-2 mx-3 my-4">
           {/* Creates link to deployed project */}
-          <div className="d-flex justify-content-center align-items-center project-links-div">
-            <a
-              className="deployed-link d-flex"
-              target="_blank"
-              href={project.deployedURL}
-              rel="noopener noreferrer"
-            >
-              {project.name}
-            </a>
-
-            {/* Creates link to github repo of project */}
-            <a
-              className="project-github-link d-flex"
-              target="_blank"
-              href={project.githubURL}
-              rel="noopener noreferrer"
-            >
-              {/* <img
-                className="img-responsive project-github-link-image"
-                src={githubLogo}
-                alt="link to github repository"
-              /> */}
-            </a>
+          <div className="d-flex justify-content-center align-items-center project-links-div mt-1">
+           <h4>{project.name}</h4>
           </div>
 
-          <div className="d-flex justify-content-center">
+          <div className="d-flex justify-content-center mb-2">
             <img
               src={project.screenshot}
               alt={project.imageText}
-              style={{ width: "240px", height: "200px" }}
+              style={{
+                width: "inherit",
+                maxWidth: "100%",
+                height: "auto",
+                borderRadius: "8px",
+              }}
+              onClick={() => {
+                window.open(project.githubURL, "_blank");
+              }}
             />
           </div>
         </div>
