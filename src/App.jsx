5fc3c4017b3dd505fa31468df70fc27970668ff5
unknown <anthony_rosati@yahoo.com>
2024-10-02 23:18:05 -0500

    Footer styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "./Components/Navigation";
+import Footer from "./Components/Footer";
 
 export default function App() {
   return (
-    <div style={{ overflowX: 'hidden'}}>
-      <header style={{ paddingTop: '1rem'}}>
-        <h3>
-          <Navigation />
-        </h3>
-      </header>
+    <div
+      style={{
+        overflowX: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <div style={{ flexGrow: 1 }}>
+        <header style={{ paddingTop: "1rem" }}>
+          <h3>
+            <Navigation />
+          </h3>
+        </header>
 
-      <main className="main" style={{ paddingTop: '5rem'}}>
-        <Outlet />
-      </main>
+        <main className="main" style={{ paddingTop: "5rem" }}>
+          <Outlet />
+        </main>
+      </div>
+
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,1 +1,49 @@
-export default function Footer() {}
+// Import logo images
+import githubLogo from "../assets/images/footerImages/github.jpg";
+import codewarsLogo from "../assets/images/footerImages/codewars.jpg";
+import linkedinLogo from "../assets/images/footerImages/linkedin.jpg";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <ul className="row footer-logos" style={{ listStyle: "none" }}>
+        {/* Render individual logos with appropriate links, opens new tab when clicked */}
+        <li className="col-4">
+          <span target="_blank" href="https://github.com/AnthonyRqqq">
+            <img
+              className="img-responsive"
+              src={githubLogo}
+              alt="github image with link"
+            />
+          </span>
+        </li>
+        <li className="col-4">
+          <span
+            target="_blank"
+            href="https://www.linkedin.com/in/anthony-rosati-30b616149/"
+          >
+            <img
+              className="img-responsive"
+              src={linkedinLogo}
+              alt="linkedin image with link"
+            />
+          </span>
+        </li>
+        <li className="col-4">
+          <span
+            onClick={() => window.open()}
+            target="_blank"
+            href="https://www.codewars.com/users/AnthonyRqqq"
+          >
+            <img
+              className="img-responsive"
+              src={codewarsLogo}
+              alt="codewars image with link"
+            />
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+}
