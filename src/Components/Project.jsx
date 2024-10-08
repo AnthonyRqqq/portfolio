9bb724832b185416871e30f0c5bdaebf9a747fc5
unknown <anthony_rosati@yahoo.com>
2024-10-08 18:50:56 -0500

    Adjust project cards to show github logo and deployed site

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,4 +1,5 @@
 import "./Project.css";
+import githubLogo from "../assets/images/footerImages/github.jpg";
 
 export default function Project({ projects }) {
   return (
@@ -12,8 +13,21 @@ export default function Project({ projects }) {
       {projects.map((project) => (
         <div key={project.id} className="project-card col-3 px-2 mx-3 my-4">
           {/* Creates link to deployed project */}
-          <div className="d-flex justify-content-center align-items-center project-links-div mt-1">
-           <h4>{project.name}</h4>
+          <div className="d-flex justify-content-center align-content-center project-links-div mt-1">
+            <h4
+              className="project-title"
+              onClick={() => window.open(project.deployedURL, "_blank")}
+            >
+              {project.name}
+            </h4>
+            <div>
+              <img
+                style={{ width: "2rem", marginLeft: "0.5rem" }}
+                className="github-logo"
+                onClick={() => window.open(project.githubURL, "_blank")}
+                src={githubLogo}
+              ></img>
+            </div>
           </div>
 
           <div className="d-flex justify-content-center mb-2">
@@ -26,9 +40,9 @@ export default function Project({ projects }) {
                 height: "auto",
                 borderRadius: "8px",
               }}
-              onClick={() => {
-                window.open(project.githubURL, "_blank");
-              }}
+              // onClick={() => {
+              //   window.open(project.githubURL, "_blank");
+              // }}
             />
           </div>
         </div>
