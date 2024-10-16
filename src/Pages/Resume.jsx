444930acc30f1d6129cf720fc284e51cc2c41e00
unknown <anthony_rosati@yahoo.com>
2024-10-15 20:37:33 -0500

    Update proficiencies list

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,5 +1,5 @@
 import resumeDownload from "../assets/Anthony-Rosati-Resume.pdf";
-import './Resume.css'
+import "./Resume.css";
 
 export default function Resume() {
   return (
@@ -12,7 +12,13 @@ export default function Resume() {
         </a>
       </h4> */}
 
-      <div style={{ display: "flex", justifyContent: "space-evenly", flexWrap: 'wrap' }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+        }}
+      >
         <div className="proficiency-card mx-2 my-2  col-lg-4 col-md-6 col-sm-6">
           {/* Front end skills list */}
           <div className="d-flex justify-content-center">
@@ -25,9 +31,15 @@ export default function Resume() {
               <li>JavaScript</li>
               <li>JQuery</li>
               <li>Responsive Design</li>
-              <li>React</li>
-              <li>Bootstrap</li>
-              <li>Prime React</li>
+              <li>Playwright End-to-End Testing</li>
+              <li>
+                <strong>Frameworks:</strong>
+                <ul>
+                  <li>React</li>
+                  <li>Bootstrap</li>
+                  <li>Prime React</li>
+                </ul>
+              </li>
             </ul>
           </div>
         </div>
@@ -42,10 +54,20 @@ export default function Resume() {
               <li>APIs</li>
               <li>Node</li>
               <li>Express</li>
-              <li>MySQL, Sequelize, MSSQL, Knex</li>
               <li>MongoDB, Mongoose</li>
               <li>REST</li>
               <li>GraphQL</li>
+              <li>Jest Unit Testing</li>
+              <li>
+                <strong>SQL</strong>
+                <ul>
+                  <li>MySQL</li>
+                  <li>Sequelize</li>
+                  <li>MSSQL</li>
+                  <li>SSRS</li>
+                  <li>Knex</li>
+                </ul>
+              </li>
             </ul>
           </div>
         </div>
